a16e5a46143ac679965dc93bcf07fe1ef59510ad
Jamesgit22 <jschroeder97@icloud.com>
2023-05-01 17:13:01 -0500

    changed functions in view all file to promises in order to call init function automatically

--- app.js
+++ app.js
@@ -1,69 +1,77 @@
 const inquirer = require("inquirer");
-const mysql = require("mysql2");
-const view = require('./cli_prompts/view_all.js')
-const addNew = require('./cli_prompts/add_all.js')
+const view = require("./cli_prompts/view_all.js");
+const addNew = require("./cli_prompts/add_all.js");
 
 // Array of possible questions to ask the use?
 // view all departments, view all roles, view all employees, add a department, add a role, add an employee, and update an employee role
 
-const init = () => {
-inquirer
-  .prompt([
-    {
-        type: 'list',
-        message: 'Please select an option.',
-        choices: ['View All Departments', 'View All Roles', 'View All Employees', 'Add a Department', 'Add a Role', 'Add an Employee', 'Update an Employee Role', 'Quit'],
-        name: 'startChoice'
-    },
-  ])
-  .then((answers) => {
-    // switch statement to determine the next prompt.
-    console.log(answers.startChoice);
+const init = async () => {
+  try {
+    const answers = await inquirer.prompt([
+      {
+        type: "list",
+        message: "Please select an option.",
+        choices: [
+          "View All Departments",
+          "View All Roles",
+          "View All Employees",
+          "Add a Department",
+          "Add a Role",
+          "Add an Employee",
+          "Update an Employee Role",
+          "Quit",
+        ],
+        name: "startChoice",
+      },
+    ]);
     switch (answers.startChoice) {
-        case 'View All Departments':
-            console.log('1')
-            view.department('name', 'department');
-            init();
-            break;
-        case 'View All Roles':
-            console.log('2')
-            view.role('title', 'salary', 'role');
-            init();
-            break;
-        case 'View All Employees':
-            console.log('3')
-            view.employee('first_name', 'last_name', 'employee');
-            init();
-            break;
-        case 'Add a Department':
-            console.log('4');
-            addNew.department();
-            break;
-        case 'Add a Role':
-            console.log('5')
-            addNew.role();
-            break;
-        case 'Add an Employee':
-            console.log('6')
-            break;
-        case 'Update an Employee Role':
-            console.log('7')
-            break;
-        case 'Quit':
-            console.log('8')
-            break;
-            
-        
+      case "View All Departments":
+        console.log("1");
+        const depResults = await view.department("name", "department");
+        console.table(depResults);
+        break;
+      case "View All Roles":
+        console.log("2");
+        const roleResults = await view.role("title", "salary", "role");
+        console.table(roleResults);
+        break;
+      case "View All Employees":
+        console.log("3");
+        const empResults = await view.employee(
+          "first_name",
+          "last_name",
+          "employee"
+        );
+        console.table(empResults);
+        break;
+      case "Add a Department":
+        console.log("4");
+        await addNew.department();
+        break;
+      case "Add a Role":
+        console.log("5");
+        await addNew.role();
+        break;
+      case "Add an Employee":
+        console.log("6");
+        // await
+        break;
+      case "Update an Employee Role":
+        console.log("7");
+        // await
+        break;
+      case "Quit":
+        console.log("8");
+        // await
+        break;
     }
-
-    })
-  .catch((error) => {
-    if (error.isTtyError) {
-      // Prompt couldn't be rendered in the current environment
-    } else {
-      // Something else went wrong
+  } catch (err) {
+    if (err) {
+      throw err;
     }
-  });
+  } finally {
+    init();
+  }
 };
 
 init();
--- cli_prompts/view_all.js
+++ cli_prompts/view_all.js
@@ -1,28 +1,44 @@
 // View all employees in the db
-const db = require('../connections/connection.js');
+const db = require("../connections/connection.js");
 
 const department = (col1, table) => {
-db.query('SELECT id,?? FROM ??', [col1, table], function (err, results) {
-    console.log('');
-    console.table(results);
-    return results;
-});
+  return new Promise((resolve, reject) => {
+    db.query("SELECT id,?? FROM ??", [col1, table], function (err, results) {
+      if (err) {
+        throw err;
+      } else {
+        console.log("");
+        console.log(results);
+        resolve(results);
+      }
+    });
+  });
 };
 
 const role = (col1, col2, table) => {
-    db.query('SELECT id,??,?? FROM ??', [col1, col2, table], function (err, results) {
-        console.log('');
-        console.table(results);
-        return results;
-    });
-    };
+  return new Promise((resolve, reject) => {
+    db.query(
+      "SELECT id,??,?? FROM ??",
+      [col1, col2, table],
+      function (err, results) {
+        console.log("");
+        resolve(results);
+      }
+    );
+  });
+};
 
 const employee = (col1, col2, table) => {
-    db.query('SELECT id,??,??,?? FROM ??', [col1, col2, table], function (err, results) {
-        console.log('');
-        console.table(results);
-        return results;
-    });
-    };
+  return new Promise((resolve, reject) => {
+    db.query(
+      "SELECT id,??,?? FROM ??",
+      [col1, col2, table],
+      function (err, results) {
+        // console.log("");
+        resolve(results);
+      }
+    );
+  });
+};
 
-module.exports = { department, role, employee}
+module.exports = { department, role, employee };
